b5d8487a01ab952e46e17d8c58db79c3cb044d30
Satvir <satvirsingh30@gmail.com>
2023-01-14 11:03:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1343,12 +1343,12 @@ if ( 'mediaSession' in navigator ) {
 		  artist: track_artist.textContent,
 		album: 'Dope Music',
           	artwork: [
-            { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '96x96', type: 'image/png' },
+            { src: 'https://www.dropbox.com/s/9s99pr2e5lv2b4j/1.png?dl=1', sizes: '96x96', type: 'image/png' }/*,
                   { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '128x128', type: 'image/png' },
                    { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '192x192', type: 'image/png' },
                    { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '256x256', type: 'image/png' },
                    { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '384x384', type: 'image/png' },
-                   { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '512x512', type: 'image/png' }
+                   { src: 'https://assets.codepen.io/4358584/1.300.jpg', sizes: '512x512', type: 'image/png' }*/
           ]
 		  //album: track_name.textContent,
 		 // artwork: track_art.style.backgroundImage

99bd776d66fda986021fd6b39bc82543afec4a8e
Satvir <satvirsingh30@gmail.com>
2025-10-14 11:53:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let now_playing = document.querySelector('.now-playing');
 let track_art = document.querySelector('.track-art');
-let track_name = document.querySelector('.track-name');
-let track_artist = document.querySelector('.track-artist');
+let track_name = document.querySelector('.track-name-text');
+let track_artist = document.querySelector('.track-artist-text');
 let track_released = document.querySelector('.track-released');
 
 

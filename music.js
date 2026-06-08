4040f48663e9ee3ee0a21eb8539a412ca8201550
Satvir <satvirsingh30@gmail.com>
2026-06-08 08:24:05 -0500

    Update music.js

--- music.js
+++ music.js
@@ -51,6 +51,14 @@ const music_list = [
          music : 'https://p320.djpunjab.is/data/320/59585/310652/BLACK%20BULL%20-%20Amrit%20Maan.mp3'
            },
     {
+         img : 'https://cover.djpunjab.is/59585/300x700/maan-made-ep-amrit-maan.jpg',
+         name : 'SUPER BUSY',
+         artist : 'Amrit Maan',
+         album : 'Amrit Maan',
+         released : 'May-25-2026',
+         music : 'https://p320.djpunjab.is/data/320/59585/310651/SUPER%20BUSY%20-%20Amrit%20Maan.mp3'
+           },
+    {
          img : 'https://cover.djpunjab.is/59568/300x700/enigma-arjan-dhillon.jpg',
          name : 'Enigma',
          artist : 'Arjan Dhillon',
